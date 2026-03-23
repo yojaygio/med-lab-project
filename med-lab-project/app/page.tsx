bfcc004e2eee716d6cab3c4436788dae058afcd0
bvillegas22 <bvillegas@addu.edu.ph>
2026-03-23 10:32:22 +0800

    fix: force dynamic rendering

--- med-lab-project/app/page.tsx
+++ med-lab-project/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+export const dynamic = 'force-dynamic';
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
